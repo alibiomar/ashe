93886fb08b62de909a001adaf637cc76609cc1df
alibiomar <omar.alibi@etudiant-enit.utm.tn>
2025-02-03 13:02:47 +0100

    #

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `default-src 'self'; script-src 'self' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data:; connect-src 'self' https://*.firebaseio.com;`
+          }
+        ],
+      },
+    ];
+  },
   devIndicators: {
     buildActivity: false,
   },
