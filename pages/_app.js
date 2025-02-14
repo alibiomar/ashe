434e8d6ae79e58a765946772eddf3a95a53f6f68
alibiomar <omar.alibi@etudiant-enit.utm.tn>
2025-02-14 15:38:31 +0100

    _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,25 @@
+import { useEffect } from 'react';
 import { AuthProvider } from '../contexts/AuthContext';
+import { BasketProvider } from '../contexts/BasketContext';
+import AcceptCookiesPopup from '../components/AcceptCookiesPopup';
 import '../styles/globals.css';
-import AcceptCookiesPopup from '../components/AcceptCookiesPopup'; 
-import { BasketProvider } from '../contexts/BasketContext'; // Import BasketProvider
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.register('/sw.js')
+        .then(() => console.log('✅ Service Worker Registered'))
+        .catch((err) => console.error('❌ SW Registration Failed', err));
+    }
+  }, []);
+
   return (
     <AuthProvider>
       <BasketProvider>
-      <Component {...pageProps} />
-      <AcceptCookiesPopup />
+        <Component {...pageProps} />
+        <AcceptCookiesPopup />
       </BasketProvider>
-
     </AuthProvider>
   );
 }
