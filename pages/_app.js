ade94591d0922761e82f9ca69bf3f0c03669ae4f
alibiomar <omar.alibi@etudiant-enit.utm.tn>
2025-02-15 18:06:39 +0100

    app

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,3 @@
-import { useEffect } from 'react';
 import { AuthProvider } from '../contexts/AuthContext';
 import { BasketProvider } from '../contexts/BasketContext';
 import AcceptCookiesPopup from '../components/AcceptCookiesPopup';
@@ -6,20 +5,17 @@ import '../styles/globals.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    if("serviceWorker" in navigator) {
-      window.addEventListener("load", function () {
-       navigator.serviceWorker.register("/sw.js").then(
-          function (registration) {
-            console.log("Service Worker registration successful with scope: ", registration.scope);
-          },
-          function (err) {
-            console.log("Service Worker registration failed: ", err);
-          }
-        );
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registered with scope:", registration.scope);
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
       });
-    }
-  }, [])
+  }
+  
   return (
     <AuthProvider>
       <BasketProvider>
