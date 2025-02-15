b44315e59b95819ca04f0698db79b9451be617fc
alibiomar <omar.alibi@etudiant-enit.utm.tn>
2025-02-15 15:28:14 +0100

    sw

--- pages/_app.js
+++ pages/_app.js
@@ -7,17 +7,19 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("/sw.js")
-        .then((registration) => {
-          console.log("Service Worker registered with scope:", registration.scope);
-        })
-        .catch((err) => {
-          console.error("Service Worker registration failed:", err);
-        });
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+       navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
     }
-  }, []);
+  }, [])
   return (
     <AuthProvider>
       <BasketProvider>
