d72e7da3eeb9906bc4754a05df511454c0926955
alibiomar <omar.alibi@etudiant-enit.utm.tn>
2025-02-18 11:14:59 +0100

    Register service worker for improved offline capabilities

--- pages/_app.js
+++ pages/_app.js
@@ -58,7 +58,18 @@ function AppContent({ Component, pageProps }) {
   if (loading) {
     return <LoadingSpinner />;
   }
-
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker.register('/sw.js')
+        .then(registration => {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        })
+        .catch(error => {
+          console.error('ServiceWorker registration failed:', error);
+        });
+    });
+  }
+  
   return (
     <div translate="yes">
       <Component {...pageProps} />
