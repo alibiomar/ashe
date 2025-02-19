f2cd3333c6a1dfeed9a7955be6503a16827b4389
alibiomar <omar.alibi@etudiant-enit.utm.tn>
2025-02-19 15:17:55 +0100

    Update Content Security Policy; allow blob URLs for images

--- next.config.js
+++ next.config.js
@@ -36,7 +36,7 @@ const config = withPWA({
               "connect-src 'self' https://fastly.picsum.photos/ https://picsum.photos/ https://fonts.gstatic.com/s/montserrat/ https://auth.ashe.tn/auth/verify-email https://auth.ashe.tn/auth/send-password-reset https://auth.ashe.tn https://*.firebaseio.com https://*.googleapis.com https://identitytoolkit.googleapis.com https://apis.google.com",
               "script-src 'self' 'unsafe-eval' https://www.gstatic.com https://apis.google.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-              "img-src 'self' data: https://*.googleusercontent.com https://dl.dropboxusercontent.com https://firebasestorage.googleapis.com https://picsum.photos https://fastly.picsum.photos",
+              "img-src 'self' data: blob: https://*.googleusercontent.com https://dl.dropboxusercontent.com https://firebasestorage.googleapis.com https://picsum.photos https://fastly.picsum.photos",
               "frame-src 'self' https://securetoken.googleapis.com https://ashe-comm.firebaseapp.com",
               "form-action 'self'",
               "font-src 'self' https://fonts.gstatic.com https://res.cloudinary.com data:",
